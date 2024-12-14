7ebc14bebcd2a4e24fa2540653d17ef258104443
Anderson Moises Taya Ipanaque <andersonty2106@gmail.com>
2024-12-14 11:42:03 -0500

    Change: Anderson/main.py

--- anderson/main.py
+++ anderson/main.py
@@ -8,8 +8,16 @@ Usa un bucle for o while para recorrer la lista.
 """
 def run():
     print("Hello Anderson")
+    def investir_lista(lista):
+        lista_invertida = []
+        for elemento in lista:
+            lista_invertida.insert(0, elemento)
+        return lista_invertida
 
-
+    lista_pedida = [1,2,3,4]
+    lista_pedida_invertida = investir_lista(lista_pedida)
+    print("Lista original: ", lista_pedida)
+    print("Lista invertida: ", lista_pedida_invertida)
 
 if __name__=='__main__':
     run()
