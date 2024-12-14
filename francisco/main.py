8e82aa08f0c904ed38b199f97a718baae97c924f
ausubel <ausubelvc@gmail.com>
2024-12-14 11:09:35 -0500

    fix: Add main.py to francisco

---
+++ francisco/main.py
@@ -0,0 +1,15 @@
+"""
+Descripción: Pide al usuario que ingrese una cadena de texto y determina cuántas vocales (a, e, i, o, u) contiene. Muestra el número total de vocales encontradas.
+Pistas:
+
+Puedes convertir la cadena a minúsculas con .lower().
+Itera sobre cada carácter y comprueba si es una vocal.
+Incrementa un contador para cada vocal encontrada.
+"""
+def run():
+    print('Hello Francisco Virgolini from main')
+
+
+
+if __name__=='__main__':
+    run()
