4ff277bcf16c346223c30ae7f6148876187ded52
ausubel <ausubelvc@gmail.com>
2024-12-14 12:41:05 -0500

    Mejorando fórmula

--- manuel/main.py
+++ manuel/main.py
@@ -2,7 +2,7 @@
 Descripción: Solicita un número entero positivo n y calcula la suma de todos los enteros desde 1 hasta n. Muestra el resultado.
 Pistas:
 
-Puedes usar un bucle for o la función sum() con range().
+Puedes usar un bucle for o la función sum() con range() o la fórmula.
 Por ejemplo, si el usuario ingresa 5, la salida debería ser 1+2+3+4+5 = 15.
 """
 def run():
@@ -19,7 +19,7 @@ def run():
             print("Error al ingresar el número. Intente nuevamente.")
 
     # Inicio de la suma
-    suma = sum(range(entero + 1))
+    suma = entero * (entero + 1) // 2
 
     # Resultado
     print(f"Se ha ingresado el número {entero} y la suma del 1 hasta dicho número es {suma}.")
